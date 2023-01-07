028ed6f5d75d77fe99b1ac87fbe065765f1eafc0
Claire <119949731+ClairePaek@users.noreply.github.com>
2023-01-07 14:18:16 +0900

    Update JadenCase 문자열 만들기.js

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,6 +1,6 @@
 function solution(s) {
     return s.split(' ').map(word => {
         const lowerCase = word.toLowerCase();
-        return `${lowerCase.charAt(0).toLocaleUpperCase()}${lowerCase.slice(1)}`
+        return `${lowerCase.charAt(0).toUpperCase()}${lowerCase.slice(1)}`
     }).join(' ');
 }
